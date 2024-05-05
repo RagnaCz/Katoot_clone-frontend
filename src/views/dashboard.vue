f1ffed019eb4d941f53f3a54cad217d5e82c6758
RagnaCz <97277959+RagnaCz@users.noreply.github.com>
2024-05-05 21:32:42 +0700

    fix(frontend): sidebar

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,25 +1,26 @@
 <template>
   <div class="w-screen h-screen flex">
-    <!-- Side Bar -->
-    <div class="w-[400px] h-full bg-gray-200 text-white" v-show="showSide">
-      <div class="h-[50px] bg-gray-900 justify-center items-center flex">
-        <h3 class="font-bold text-xl">Kahoot! Clone</h3>
-      </div>
-      <div class="h-[calc(100vh-50px)] bg-gray-800">
+    <transition name="sidebar-slide">
+      <!-- Side Bar -->
+      <div class="w-[400px] h-full bg-gray-200 text-white" v-show="showSide">
+        <div class="h-[50px] bg-gray-900 justify-center items-center flex">
+          <h3 class="font-bold text-xl">Kahoot! Clone</h3>
+        </div>
+        <div class="h-[calc(100vh-50px)] bg-gray-800">
 
-        <div class="flex flex-col justify-between h-full px-[20px] space-y-[10px]">
-          <div class=" flex flex-col justify-between space-y-[10px]">
-            <router-link to="/library" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-200 hover:text-gray-800  transition duration-400 ease-in-out">
-              <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path>
-              </svg>
-              Home
-            </router-link>
+          <div class="flex flex-col justify-between h-full px-[20px] space-y-[10px]">
+            <div class=" flex flex-col justify-between space-y-[10px]">
+              <router-link to="/library" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-200 hover:text-gray-800  transition duration-400 ease-in-out">
+                <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path>
+                </svg>
+                Home
+              </router-link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
+    </transition>
     <!-- Mainsection-->
     <div class="w-full h-full bg-gray-400 border-gray-500 border-b-2">
       <div class="h-[50px] bg-gray-100 flex items-center shadow-sm px-[20px] w-full py-[10px] z-10 border-b ">
@@ -100,5 +101,13 @@ export default {
 
 .sidebar-item:active {
   transform: scale(0.95);
+}
+
+.sidebar-slide-enter-active, .sidebar-slide-leave-active {
+  transition: transform 0.5s ease;
+}
+
+.sidebar-slide-enter, .sidebar-slide-leave-to {
+  transform: translateX(-100%);
 }
 </style>
