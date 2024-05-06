a6101411eda9cceee4741bd841ff4fe0dba5c095
RagnaCz <97277959+RagnaCz@users.noreply.github.com>
2024-05-06 18:34:17 +0700

    feat(frontend): fix interface (non fix minimize screen)

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -13,7 +13,7 @@ const inactiveClass = ref(
   <aside id="default-sidebar"
     class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 "
     aria-label="Sidebar">
-    <div class="h-full text-white border-gray-500" v-show="showSide">
+    <div class="h-full text-white border-gray-500">
       <div class="h-[70px] bg-indigo-950 justify-center items-left py-6">
         <p class="font-bold text-5xl">Kahoot!</p>
         <p class="font-bold text-xl">Clone</p>
@@ -22,70 +22,73 @@ const inactiveClass = ref(
 
         <div class=" justify-between items-center w-full h-full  space-y-[10px]">
           <ul class="space-y-2 font-medium flex flex-col justify">
-            <li><br></li><li><br></li>
+            <li><br></li>
+            <li><br></li>
             <div class="flex flex-col justify-between px-5">
-              <button type="button" class="sidebar-button text-white hover:text-white border border-white hover:bg-gray-900 focus:outline-none font-bold rounded-lg text-2xl w-full py-4 text-center me-2 mb-2 dark:border-white dark:text-white dark:hover:text-indigo-950 dark:hover:bg-white">
+              <button type="button"
+                class="sidebar-button text-white hover:text-white border border-white hover:bg-gray-900 focus:outline-none font-bold rounded-lg text-2xl w-full py-4 text-center me-2 mb-2 dark:border-white dark:text-white dark:hover:text-indigo-950 dark:hover:bg-white">
                 + CREATE
               </button>
             </div>
             <li>
-              <router-link class="flex items-center px-6 py-3 mt-4 duration-200 border-l-4"
+              <router-link @click="scrollToTop" class="flex items-center px-6 py-3 mt-4 duration-200 border-l-4"
                 :class="[$route.name === 'library' ? activeClass : inactiveClass]" to="/library">
-                <svg class="w-5 h-5" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M2 10C2 5.58172 5.58172 2 10 2V10H18C18 14.4183 14.4183 18 10 18C5.58172 18 2 14.4183 2 10Z"
-                    fill="currentColor" />
-                  <path d="M12 2.25195C14.8113 2.97552 17.0245 5.18877 17.748 8.00004H12V2.25195Z" fill="currentColor" />
+                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                  width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                  <path fill-rule="evenodd"
+                    d="M5.005 10.19a1 1 0 0 1 1 1v.233l5.998 3.464L18 11.423v-.232a1 1 0 1 1 2 0V12a1 1 0 0 1-.5.866l-6.997 4.042a1 1 0 0 1-1 0l-6.998-4.042a1 1 0 0 1-.5-.866v-.81a1 1 0 0 1 1-1ZM5 15.15a1 1 0 0 1 1 1v.232l5.997 3.464 5.998-3.464v-.232a1 1 0 1 1 2 0v.81a1 1 0 0 1-.5.865l-6.998 4.042a1 1 0 0 1-1 0L4.5 17.824a1 1 0 0 1-.5-.866v-.81a1 1 0 0 1 1-1Z"
+                    clip-rule="evenodd" />
+                  <path
+                    d="M12.503 2.134a1 1 0 0 0-1 0L4.501 6.17A1 1 0 0 0 4.5 7.902l7.002 4.047a1 1 0 0 0 1 0l6.998-4.04a1 1 0 0 0 0-1.732l-6.997-4.042Z" />
                 </svg>
-
                 <span class="mx-4 font-bold text-3xl">Library</span>
               </router-link>
             </li>
             <li>
-              <router-link class="flex items-center px-6 py-3 mt-4 duration-200 border-l-4"
+              <router-link @click="scrollToTop" class="flex items-center px-6 py-3 mt-4 duration-200 border-l-4"
                 :class="[$route.name === 'history' ? activeClass : inactiveClass]" to="/history">
-                <svg class="w-5 h-5" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M2 10C2 5.58172 5.58172 2 10 2V10H18C18 14.4183 14.4183 18 10 18C5.58172 18 2 14.4183 2 10Z"
-                    fill="currentColor" />
-                  <path d="M12 2.25195C14.8113 2.97552 17.0245 5.18877 17.748 8.00004H12V2.25195Z" fill="currentColor" />
+                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                  width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                  <path fill-rule="evenodd"
+                    d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
+                    clip-rule="evenodd" />
                 </svg>
-
                 <span class="mx-4 font-bold text-3xl">History</span>
               </router-link>
-            </li> 
+            </li>
             <li>
-              <router-link class="flex items-center px-6 py-3 mt-4 duration-200 border-l-4"
+              <router-link @click="scrollToTop" class="flex items-center px-6 py-3 mt-4 duration-200 border-l-4"
                 :class="[$route.name === 'config' ? activeClass : inactiveClass]" to="/config">
-                <svg class="w-5 h-5" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M2 10C2 5.58172 5.58172 2 10 2V10H18C18 14.4183 14.4183 18 10 18C5.58172 18 2 14.4183 2 10Z"
-                    fill="currentColor" />
-                  <path d="M12 2.25195C14.8113 2.97552 17.0245 5.18877 17.748 8.00004H12V2.25195Z" fill="currentColor" />
+                <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                  width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                    d="M20 6H10m0 0a2 2 0 1 0-4 0m4 0a2 2 0 1 1-4 0m0 0H4m16 6h-2m0 0a2 2 0 1 0-4 0m4 0a2 2 0 1 1-4 0m0 0H4m16 6H10m0 0a2 2 0 1 0-4 0m4 0a2 2 0 1 1-4 0m0 0H4" />
                 </svg>
-
                 <span class="mx-4 font-bold text-3xl">Config</span>
               </router-link>
             </li>
-
-            
           </ul>
-            <div class="flex flex-col justify-between px-5">
-              <br><br>
-              <router-link to="/library">
-                <button type="button" class="sidebar-button text-white hover:text-white border border-white hover:bg-gray-900 focus:outline-none font-bold rounded-lg text-2xl w-full py-4 text-center me-2 mb-2 dark:border-white dark:text-white dark:hover:text-indigo-950 dark:hover:bg-white">
-                  PLAY
-                </button>
-              </router-link>
-            </div>
+          <div class="flex flex-col justify-between px-5">
+            <br><br>
+            <router-link to="/library">
+              <button type="button"
+                class="sidebar-button text-white hover:text-white border border-white hover:bg-gray-900 focus:outline-none font-bold rounded-lg text-2xl w-full py-4 text-center me-2 mb-2 dark:border-white dark:text-white dark:hover:text-indigo-950 dark:hover:bg-white">
+                ▶ PLAY
+              </button>
+            </router-link>
+          </div>
         </div>
       </div>
     </div>
   </aside>
 
   <!-- Mainsection-->
-  <div class=" sm:ml-64 bg-gray-400 border-gray-500 border-b-2">
-    <header class="h-[70px] bg-gray-100 flex items-center shadow-sm px-[20px] py-[10px] z-10 border-b ">
+  <div class="sm:ml-64 bg-gray-400 border-gray-500 border-b-2">
+    <nav
+      class="flex fixed w-full z-10 top-0 start-0 h-screen-[70px] bg-gray-100 items-center shadow-sm px-[10px] py-[5px]  border-b ">
       <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar"
         type="button"
-        class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+        class="items-center p-2 m-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-indigo-950 dark:focus:ring-gray-600">
         <span class="sr-only">Open sidebar</span>
         <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg">
@@ -100,7 +103,7 @@ const inactiveClass = ref(
         <div class="w-[calc(100%-200px)]"></div>
         <!-- User login -->
         <div class="w-[200px]">
-          <div class="flex items-center justify-end space-x-4 hover:bg-gray-200 rounded-lg text-center inline-flex"
+          <div class="flex items-center justify-end space-x-4 hover:bg-gray-200 rounded-lg text-center"
             @click="toggleDrop">
             <img class="w-10 h-10 rounded-full" src="../assets/vue.svg" alt="">
             <div class="font-semibold text-gray-900 text-left">
@@ -108,7 +111,7 @@ const inactiveClass = ref(
               <div class="text-sm text-gray-500 dark:text-gray-400">{{ this.auth.currentUser.email }}</div>
             </div>
           </div>
-          
+
           <!-- Drop down -->
           <div v-show="showDropDown"
             class="absolute right-[10px] z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
@@ -122,9 +125,9 @@ const inactiveClass = ref(
           </div>
         </div>
       </div>
-    </header>
-    <div class="h-[calc(100vh-50px)] bg-gray-50 p-[20px]">
-      <div class="border border-gray-300 rounded-md p-[20px] h-full">
+    </nav>
+    <div class="h-full mt-[50px] bg-gray-50 p-[40px]">
+      <div class="min-h-[calc(100vh-70px)] ">
         <router-view></router-view>
       </div>
     </div>
@@ -139,7 +142,6 @@ export default {
     return {
       auth: getAuth(),
       showDropDown: false,
-      showSide: true
     }
   },
   methods: {
@@ -154,6 +156,12 @@ export default {
         .catch((error) => {
           alert(error.message)
         })
+    },
+    scrollToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
     }
   },
   mounted() {
