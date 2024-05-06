5d435bbb295ddc7acaaec832604d6965299a0479
Fankrbb <fankrbb@gmail.com>
2024-05-07 00:06:05 +0700

    feat(QuizList): add Quiz Card to library

---
+++ src/components/libraryComponent/QuizList.vue
@@ -0,0 +1,103 @@
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite();
+})
+</script>
+
+<template>
+  <div v-for="quiz in quizzes" :key="quiz._id">
+    <div
+      class="text-left w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 justify-between">
+      <div class="p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800">
+        <div class="flex items-center justify-between">
+          <h2 class="mb-3 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">{{ quiz.quiz_name }}</h2>
+          <button id="dropdownModifyQuizBtn" data-dropdown-toggle="dropdownModifyQuiz" class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button"> 
+            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
+              <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
+            </svg>
+          </button>
+          
+          <!-- Dropdown menu -->
+          <div id="dropdownModifyQuiz" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+              <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownModifyQuizBtn">
+                <li>
+                  <a :href="`/library/${quiz._id}`" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                </li>
+                <li>
+                  <div @click="deleteQuiz(quiz._id)" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</div>
+                </li>
+              </ul>
+          </div>
+          
+        </div>
+
+
+        <div class="flex items-center mb-3 text-gray-500 dark:text-gray-400">
+          <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+            width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+              d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5" />
+          </svg>
+          {{ quiz.question_count }} items
+        </div>
+        <div class="flex items-center justify-between">
+          <p class="text-left">Create at: {{ quiz.create_at }}</p>
+          <div class="flex items-center">
+            <button type="button"
+              class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
+              <svg class="w-[18px] h-[18px] text-gray-800 dark:text-white" aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M5 11.917 9.724 16.5 19 7.5" />
+              </svg>
+              <span class="sr-only">Icon description</span>
+            </button>
+            <button type="button"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              <svg class="w-[18px] h-[18px] text-gray-800 dark:text-white" aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M8.6 5.2A1 1 0 0 0 7 6v12a1 1 0 0 0 1.6.8l8-6a1 1 0 0 0 0-1.6l-8-6Z"
+                  clip-rule="evenodd" />
+              </svg>
+              <span class="sr-only">Icon description</span>
+            </button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuizList',
+  data() {
+    return {
+      quizzes: [
+        {
+          _id: "a299012830293",
+          quiz_name: "q_name1",
+          question_count: 3,
+          create_at: "12 13 14"
+        },
+        {
+          _id: "asdioneqpw9eqwe",
+          quiz_name: "q_name2",
+          question_count: 2,
+          create_at: "12 13 14"
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteQuiz(quiz_id) {
+
+    },
+  }
+}
+</script>
