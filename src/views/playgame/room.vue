cfce516acb85f7d462ebae6244a41834c64f0d5d
RagnaCz <97277959+RagnaCz@users.noreply.github.com>
2024-05-08 21:51:01 +0700

    fix(frontend): fix Empty Quiz no Add button

---
+++ src/views/playgame/room.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+onMounted(() => {
+    initFlowbite();
+})
+
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Room',
+    data() {
+        return {
+            roompin: '' 
+        }
+    },
+    created() {
+        this.roompin = this.$route.params.roompin;
+    }
+}
+</script>
