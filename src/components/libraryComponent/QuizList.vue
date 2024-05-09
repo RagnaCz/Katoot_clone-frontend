ea920e1626456b66ef7972a6e198055115336298
RagnaCz <97277959+RagnaCz@users.noreply.github.com>
2024-05-09 22:46:28 +0700

    fix(frontend): Dropdown edit fix

--- src/components/libraryComponent/QuizList.vue
+++ src/components/libraryComponent/QuizList.vue
@@ -70,9 +70,9 @@ onMounted(() => {
           <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
             Create Quiz
           </h3>
-          <button type="button" @click="cancelCreate"
+          <button type="button" @click="toggleModal"
             class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-            data-modal-hide="createRoomModal">
+            data-modal-target="createRoomModal" data-modal-hide="createRoomModal">
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                 d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
@@ -100,13 +100,6 @@ onMounted(() => {
       </div>
     </div>
   </div>
-
-
-
-
-
-
-
 </template>
 
 <script>
@@ -121,6 +114,7 @@ export default {
   data() {
     return {
       auth: getAuth(),
+      modal: '',
       quizzes: [],
       formData: {
         time_limit: 60,
@@ -132,13 +126,16 @@ export default {
   methods: {
     selectQuiz(quiz_id) {
       this.selectedQuiz = quiz_id
+      this.toggleModal()
       console.log(this.selectedQuiz)
     },
     cancelCreate() {
       this.selectedQuiz = ''
     },
+    toggleModal() {
+      this.modal.toggle();
+    },
     deleteQuiz(quiz_id) {
-
       this.auth.currentUser.getIdToken().then((token) => {
         axios.delete(import.meta.env.VITE_BACKEND_URI + '/api/quizzes/' + quiz_id, {
           withCredentials: true,
@@ -195,6 +192,9 @@ export default {
           }
         }).catch((err) => { console.log(err) })
     })
+    // set the modal menu element
+    const popuptarget = document.getElementById('createRoomModal');
+    this.modal = new Modal(popuptarget);
   }
 }
 </script>
